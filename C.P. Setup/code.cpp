9b49478693b5b905413ed6040a8cf2cb236eda2d
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-04-09 00:44:25 +0530

    cp starting basic

---
+++ C.P. Setup/code.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+#define INPUT_OUTPUT { \
+	freopen("input.txt", "r", stdin); \
+	freopen("output.txt", "w", stdout); \
+}*/  //INPUT_OUTPUT;
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int a, b;
+	 cin>>a>>b;
+
+	cout<<a+b<<endl;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+int main(void)
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int x=23;
+	
+	cin >> x;
+
+	cout << x;
+	return 0;	
+}
+
+*/
+
