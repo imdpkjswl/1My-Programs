d6c596e36204508e565d7392fcdff01ef1b0e43f
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-05-21 00:00:20 +0530

    Coin change problem perfect soln

---
+++ ALGORITHMS/JAVA/5.Greedy Technique/1.Coin Change Problem using GreedyL2B2/src/Main.java
@@ -0,0 +1,63 @@
+/**
+ * !!!! Coins array must be sorted in Non-Increasing Order !!!!
+ * Note: Greedy Algorithms does not guarantee Optimal Solution.
+ *  If m != 0, Then Solution does not exist.
+ */
+
+public class Main {
+
+    static int[] ans = new int[100]; // store Optimal solution.
+
+    public static int findMinCoins(int []coins, int size,  int m) {
+        int i, count = 0;
+
+        for(i = 0; i < size; i++)
+        {
+            //take as much from coins[i]
+            while(m >= coins[i])
+            {
+                //after taking the coin, reduce the value.
+                m -= coins[i];
+                ans[count++] = coins[i];
+                //count++;
+            }
+            if(m == 0)
+                break;
+        }
+
+        if(m != 0) {
+            System.out.println("Solution does not exist.");
+            System.exit(0);
+        }
+        return count;
+    }
+
+    public static void  main(String [] args)
+    {
+        /* Example for solution does not exist  */
+//        int[] coins = {10,5,2};
+//        int i, m = 11;
+
+        /* Example for solution is an optimal solution */
+        int[] coins = {25,10,5,1};
+        int i, m = 48;
+
+        /* Example for will not guarantee optimal solution */
+//        int[] coins = {25,15,1};
+//        int i, m = 30;
+
+
+        //find the size of the coins array
+        int size = coins.length;
+
+        int MinCount = findMinCoins(coins,size,m);
+
+        System.out.printf("Total Coins Needed = %d\n",MinCount);
+
+        System.out.printf("Coins are:\t");
+        for(i = 0; i < MinCount; i++)
+            System.out.printf("%d ", ans[i]);
+
+
+    }
+}
