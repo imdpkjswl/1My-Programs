80ebc2d63e6996c1b8bac4a7f4134b999b00d393
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-05-21 21:44:49 +0530

    bug:Notworking for large capacity

--- ALGORITHMS/JAVA/5.Greedy Technique/2.Fractional KnapSack Problem using Greedy/src/fractionalKnapsackGreedyTechnique.java
+++ ALGORITHMS/JAVA/5.Greedy Technique/2.Fractional KnapSack Problem using Greedy/src/fractionalKnapsackGreedyTechnique.java
@@ -1,21 +1,28 @@
 import java.util.*;
 
 public class fractionalKnapsackGreedyTechnique {
-    static int n, m, rc;            // rc is remaining capacity
+    static Scanner in = new Scanner(System.in);
+    static{
+        System.out.println("Enter the number of instance:");
+        n =in.nextInt();
+    }
+    static int n, m;
+    static float rc;            // rc is remaining capacity
     static float[] w = new float[n];
     static float[] p = new float[n];
     static float[] x = new float[n];  // contain solutions
     static float[] pw = new float[n];
 
     // finding pi/wi and arranged in decreasing order:
-    static void fractionSort(){
-        for(int i=0;i<n;i++){
+    static void fractionSort() {
+        for(int i=0;i<n;i++) {
             pw[i] = (p[i] / w[i]);  // calculating fraction of profit and weight
         }
 
-        for(int i=1;i<n;i++){
-            for(int j=0;j<n-i;j++){
-                if(pw[i] > pw[i+1]){
+        // Sorting pw in decreasing order with placing respective value in their places using bubble sort:
+        for(int j=1;j<n;j++) {
+            for(int i=0;i<n-j;i++) {
+                if(pw[i] < pw[i+1]) {
                     float temp1, temp2, temp3;
                     temp1 = pw[i];
                     pw[i] = pw[i+1];
@@ -28,26 +35,49 @@ public class fractionalKnapsackGreedyTechnique {
                     temp3 = p[i];
                     p[i] = p[i+1];
                     p[i+1] = temp3;
-
                 }
             }
         }
 
-        System.out.println(Arrays.toString(pw));
-        System.out.println(Arrays.toString(w));
+//        System.out.println(Arrays.toString(pw));
+//        System.out.println(Arrays.toString(w));
+//        System.out.println(Arrays.toString(p));
+    }
+
+    static float greedyKnapsack(){
+        float sum=0f;
+
+        for(int i=0;i<n;i++)
+            x[i] = 0;
+        rc = m;
+
+        for(int i=0;i<n;i++){
+            if(w[i] <= rc){
+                rc = rc - w[i];
+                x[i] = 1;
+            }
+            else{
+                x[i] = rc/w[i];
+                rc = rc - (w[i]*x[i]);
+            }
+
+            if(rc == 0)
+                break;
+        }
+
         System.out.println(Arrays.toString(p));
+        System.out.println(Arrays.toString(x));
+        for(int i=0;i<n;i++){
+            sum += p[i]*x[i];
+        }
+
+        return sum;
     }
 
-    static void greedyKnapsack(){
 
-    }
-
-
-
+// This program is not able to select multiple objects.Hence it will not work on large  value of capacity.
     public static void main(String args[]){
-            Scanner in = new Scanner(System.in);
-            System.out.println("Enter the number of instance:");
-            n =in.nextInt();
+
             System.out.println("Enter the weights:");
                 for(int i=0;i<n;i++)
                     w[i] = in.nextFloat();
@@ -59,8 +89,9 @@ public class fractionalKnapsackGreedyTechnique {
             rc = m;
 
             fractionSort();
+            float result = greedyKnapsack();
 
-            //System.out.println("The Total Optimal Profit Is " + sum);
+            System.out.println("The total optimal profit is:  " + result);
 
     }
 }
