37594be2350239b3c35fe1e9b390f47152f0965d
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-06-08 06:47:56 +0530

    fixed for variable length string

--- JAVA/$TopicWise/11.String/1.Anagram O(n^2) approach/src/com/company/Main.java
+++ JAVA/$TopicWise/11.String/1.Anagram O(n^2) approach/src/com/company/Main.java
@@ -1,28 +1,36 @@
 package com.company;
 
+/**
+ * This Program have O(n^2) complexity, The better ways is to solve this is, Sort the both string and compare them.
+ */
 public class Main {
 
     public static void main(String[] args) {
 
-        String a = "catact";
-        String b = "actata";
+        String a = "catac";
+        String b = "acta";
 
         boolean isAnagram = false;  // initially suppose strings are not anagram.
         boolean[] visited = new boolean[20]; // used for character frequency management of string
 
-        for(int i=0;i<a.length();i++){
-            char c = a.charAt(i);   // taking one character from string 'a' each time
-            isAnagram = false;
-            for(int j=0;j<b.length();j++){
-                if(b.charAt(j) == c  && !visited[j]){  // check string 'b' jth character is matching with ith character of string 'a'
-                    visited[j] = true;
-                    isAnagram = true;
-                    break;
+        if(a.length() == b.length()) {  // Anagram must have same length
+                for (int i = 0; i < a.length(); i++) {
+                    char c = a.charAt(i);   // taking one character from string 'a' each time
+                    isAnagram = false;
+                    for (int j = 0; j < b.length(); j++) {
+                        if (b.charAt(j) == c && !visited[j]) {  // check string 'b' jth character is matching with ith character of string 'a'
+                            visited[j] = true;
+                            isAnagram = true;
+                            break;
+                        }
+                    }
+                    if (!isAnagram)  // after
+                        break;
                 }
-            }
-            if(!isAnagram)  // after
-                break;
         }
+
+
+
         if(isAnagram)
             System.out.println("Anagram");
         else
