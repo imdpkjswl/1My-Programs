cfeb2cd0af9f37a861f9491f9f62a62eabee1df1
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-06-09 08:03:47 +0530

    Triplets using Naive & Two Pointer Algo
    AsKED IN AMaZOn

--- JAVA/$TopicWise/12.Two Point Algorithm/1.Find Tripets/src/TwoPointAlgo.java
+++ JAVA/$TopicWise/12.Two Point Algorithm/1.Find Tripets/src/TwoPointAlgo.java
@@ -1,11 +1,47 @@
 import java.util.*;
 
+/**
+ * Note: Two pointer Algorithm work only on sorted array like binary search.
+ * Time complexity : O(n^2)
+ *
+ * The Naive Approach O(n^3):
+
+                                                                     Function() {
+                                                                         for(int i=0;i<arr.length;i++){
+                                                                                for(int j=i+1;j<arr.length;j++){
+                                                                                    for(int k=j+1;k<arr.length;k++){
+                                                                                        if(arr[i] + arr[j] + arr[k] == 0){
+                                                                                            return true;
+                                                                                        }
+                                                                                   }
+                                                                                }
+                                                                         }
+                                                                         return false;
+                                                                     }
+
+        ## Another Naive Approach O(n^3):
+
+         Function() {
+            for(int i=0;i<arr.length;i++){
+                for(int j=i+1;j<arr.length;j++){
+                    for(int k=j+1;k<arr.length;k++){
+                        if(-arr[i] == arr[j] + arr[k]){
+                            return true;
+                        }
+                    }
+                }
+            }
+         return false;
+         }
+ */
+
 
 public class TwoPointAlgo {
 
     public static boolean findTripets(int[] arr, int n){
 
         Arrays.sort(arr);
+        System.out.println("Sorted array is:  "+Arrays.toString(arr));
         for(int i=0;i<n-2;i++) {
                 if(findTwoSum(arr, -arr[i], i+1))
                     return true;
@@ -16,7 +52,8 @@ public class TwoPointAlgo {
 
     private static boolean findTwoSum(int[] arr, int x, int i) {
         int j = arr.length-1;
-        while(i<j) {
+
+        while(i<=j) {
             if(arr[i] + arr[j] > x)
                 j--;
             else if(arr[i] + arr[j] < x)
@@ -33,12 +70,13 @@ public class TwoPointAlgo {
         int n;
         System.out.println("Enter the size of array");
         n = in.nextInt();
-        int[] arr = new int[20];
+        int[] arr = new int[n];
         System.out.println("Enter the elements:");
         for(int i=0;i<n;i++)
             arr[i] = in.nextInt();
 
-        System.out.println(findTripets(arr,n));
+        System.out.println("Triplet found status:  "+findTripets(arr,n));
+
     }
 
 }
