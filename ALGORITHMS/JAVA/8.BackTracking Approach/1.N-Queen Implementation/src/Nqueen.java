516b9b45301d0ef4faa7b0e1798844d3c1c9617b
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-06-10 07:29:12 +0530

    N-Queens implementation using Branch & Bound Approach

--- ALGORITHMS/JAVA/8.BackTracking Approach/1.N-Queen Implementation/src/Nqueen.java
+++ ALGORITHMS/JAVA/8.BackTracking Approach/1.N-Queen Implementation/src/Nqueen.java
@@ -6,13 +6,13 @@ public class Nqueen {
 
         for (int i = 1; i <= k - 1; i++) {
             if (x[i] == x[k] || Math.abs(i - k) == Math.abs(x[i] - x[k]))
-                return true;
+                return false;
         }
-        return false;
+        return true;
     }
 
     static void nqueens(int n){
-        int []x = new int[10];
+        int []x = new int[50];
         int count =0;
         int k = 1;
 
@@ -46,7 +46,7 @@ public class Nqueen {
     }
 
     static void print_solution(int n, int[] x){
-        char [][] c = new char[10][10];
+        char [][] c = new char[50][50];
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
@@ -54,9 +54,13 @@ public class Nqueen {
             }
         }
 
+        for(int i=1;i<=n;i++){
+            c[i][x[i]] = 'Q';
+        }
+
         for(int i =1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                System.out.print(c[i][j]);
+                System.out.print(c[i][j]+" ");
             }
             System.out.println();
         }
@@ -68,6 +72,10 @@ public class Nqueen {
         System.out.println("Enter the no. of queens:");
         n = in.nextInt();
 
+        long start = System.currentTimeMillis();
         nqueens(n);
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time taken to process in milliseconds:    "+(end-start));
     }
 }
