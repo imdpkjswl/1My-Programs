40e94e3fc616aac854284ae53590f31ceaaf42aa
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-06-18 21:18:35 +0530

    Rotate Matrix in Anti-Clockwise using temporary array

---
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/2.Matrix Operations & Spare Matrix/3.Inplace rotate square matrix by 90 degree AntiClockwise Using temporary Array/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Time Complexity: O(n^2) , due to nested loop
+ * Space Complexity: O(n^2) ,due to temporary array
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n;
+        System.out.println("Enter size n:");
+        n = in.nextInt();
+
+
+        int[][] mat = new int[n][n];  // input data
+        int[][] matR = new int[n][n]; // store result
+
+        // input as a matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = in.nextInt();
+            }
+        }
+
+
+        // Matrix rotation logic
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matR[i][j] = mat[j][n - 1 - i];
+            }
+        }
+
+        // print rotated matrix
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(matR[j][i] + "  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
