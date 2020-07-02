9450cba36e94732e22712385e9e287da044f867f
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-07-03 01:20:43 +0530

    first string is subsequence of second

---
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/4.Strings & diff searching algo/Subsequence & Substring/1.Given two strings, find if first string is a subsequence of second/src/com/iamdj/Main.java
@@ -0,0 +1,33 @@
+package com.iamdj;
+
+public class Main {
+
+    // Returns true if str1[] is a subsequence of str2[] m is length of str1 and n is length of str2
+    static boolean isSubSequence(String str1, String str2, int m, int n) {
+        int j = 0;
+        // Traverse str2 and str1, and compare current character of str2 with first
+        // unmatched char of str1, if matched then move ahead in str1
+        for (int i = 0; i < n && j < m; i++)
+            if (str1.charAt(j) == str2.charAt(i))
+                j++;
+
+        // If all characters of str1 were found in str2
+        return (j == m);
+    }
+
+    // Driver program to test methods of graph class
+    public static void main (String[] args) {
+
+        String str1 = "gksrek";
+        String str2 = "geeksforgeeks";
+        int m = str1.length();
+        int n = str2.length();
+        boolean res = isSubSequence(str1, str2, m, n);
+
+        if(res)
+            System.out.println("Yes"); // str1 sub sequence of str2.
+        else
+            System.out.println("No");
+    }
+}
+
