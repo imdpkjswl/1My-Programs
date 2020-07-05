bcc7196c78a15c9139257701281b52b11303da99
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-07-06 01:21:25 +0530

    **Important

--- MYCODE@RE@/#DSA TOPICWISE PRACTICE/4.Strings & diff searching algo/Problems on Anagram/3.Check if binary representations of two numbers are anagram/src/com/iamdj/Main.java
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/4.Strings & diff searching algo/Problems on Anagram/3.Check if binary representations of two numbers are anagram/src/com/iamdj/Main.java
@@ -1,6 +1,11 @@
 package com.iamdj;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Time complexity: O(nLog(n))
+ */
 
 public class Main {
 
@@ -43,10 +48,47 @@ public class Main {
     }
 
     // driver program
-    public static void main (String[] args) {
+    public static void main (String[] args) throws InterruptedException {
 
         long a = 5, b = 3;
 
+        long start = System.currentTimeMillis();
+        TimeUnit.SECONDS.sleep(5); // Method to stop process for a while.
+
         System.out.println(bit_anagram_check(a, b));
+
+        long end = System.currentTimeMillis();
+        System.out.println("Time elapsed:  "+(end-start));
     }
 }
+
+/** Solving using "Long.bitCount(number)" method to get no. of one's in the given number.
+ * Time complexity: O(1)
+ * Space Complexity: O(1)
+  */
+/*
+class Main {
+    // Function returns true if binary representations of a and b are anagram
+    static boolean bit_anagram_check(long a, long b) {
+        // Long.bitCount(a) gives number of 1's present in binary representation of a
+        return (Long.bitCount(a) == Long.bitCount(b));
+    }
+
+    // driver program
+    public static void main (String[] args) {
+        long a = 12, b = 7;
+
+
+        long start = System.currentTimeMillis();
+
+        if(bit_anagram_check(a, b))
+            System.out.println(true);
+        else
+            System.out.println(false);
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time elapsed:  "+(end-start));
+    }
+}
+*/
