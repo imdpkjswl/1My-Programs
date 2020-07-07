6bff534ad34d23ef5334549e07fe8f1d7033c73c
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-07-07 12:05:04 +0530

    tricky

---
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/6.Bit Manipulation/3.Add 1 to the given integer/src/com/iamdj/Main.java
@@ -0,0 +1,15 @@
+package com.iamdj;
+
+//  -~x = x + 1    //
+
+class Main {
+    public static void main(String[] args) {
+
+        int x = 4;
+        System.out.println(" "+x + " + " + 1 + " =  " + -~x);
+
+        x = -5;
+        System.out.println(x + " + " + 1 + " = " + -~x);
+
+    }
+}
