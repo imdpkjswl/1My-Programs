f1e9279854582aeba748fb2173749803ff73e224
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-07-09 00:50:30 +0530

    solved in O(logn) using fast power method

---
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/7.Recursions/3.Calculate power a^b using fast power method/src/com/iamdj/Main.java
@@ -0,0 +1,30 @@
+package com.iamdj;
+
+/**
+ * Time Complexity: O(Log(n))
+ */
+public class Main {
+
+    static int stepCounter;
+    static int fastPower(int a, int b) {
+        if(b == 0)  // Base case
+            return 1;
+
+        stepCounter++;
+        int identify = b & 1; // checking even or odd
+        if(identify == 0)  // for even power
+            return fastPower(a*a,b/2);  // relation for even power
+        else
+            return a * fastPower(a,b-1);  // relation for odd power
+    }
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 10;
+
+        System.out.println(fastPower(a,b));
+        System.out.println("Total steps needed:  "+stepCounter);
+
+    }
+}
