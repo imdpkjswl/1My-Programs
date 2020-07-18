43375ac1be9834874f96d0fa64ca2b12c68029db
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-07-18 18:14:44 +0530

    useless files deleted.

--- VTU-CSE-LAB-SOLUTIONS-Guide/6th SEM/SYSTEM SOFTWARE AND OPERATING SYSTEM LABORATORY/f1.c
+++
@@ -1,8 +0,0 @@
-#include<stido.h>
-
-int main()
-{
-	// this is a comment 
-	printf("hello");
-	/* this is another comment */
-}
--- VTU-CSE-LAB-SOLUTIONS-Guide/6th SEM/SYSTEM SOFTWARE AND OPERATING SYSTEM LABORATORY/f2.c
+++
@@ -1,6 +0,0 @@
-#include<stido.h>
-
-int main()
-{
-	printf("hello");
-}
