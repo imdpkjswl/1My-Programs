6cfbca9b1feef839ca13c968d24322032c4c7414
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-07-23 00:52:26 +0530

    Find min in BST

--- MYCODE@RE@/#DSA TOPICWISE PRACTICE/11.Binary Search Tree/5.Find the node with minimum value in a Binary Search Tree/src/Main.java
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/11.Binary Search Tree/5.Find the node with minimum value in a Binary Search Tree/src/Main.java
@@ -1,6 +1,78 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter item:");
+        data =s.nextInt();
+    }
+}
+
+class binarySearchTree {
+
+    Node insert(Node root) {
+        Node temp = new Node();
+        if(root == null) {
+            System.out.println(temp.data+" is inserted.");
+            root = temp;
+            return root;
+        }
+
+        Node cur,prev;
+        cur = root;
+        prev = null;
+        while(cur != null) {
+            prev = cur;
+            if(temp.data <= cur.data)
+                cur = cur.left;
+            else
+                cur = cur.right;
+        }
+
+        if(temp.data <= prev.data)
+            prev.left = temp;
+        else
+            prev.right = temp;
+
+        System.out.println(temp.data+" is inserted.");
+        return root;
+    }
+
+    // Find min
+    static int min = Integer.MAX_VALUE;
+    void minUsingInorder(Node root) {
+        if(root == null) return ;
+
+        minUsingInorder(root.left);
+        if (min > root.data)
+            min = root.data;
+        minUsingInorder(root.right);
+    }
+
+
+
+}
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Node root = null;
+        binarySearchTree bst = new binarySearchTree();
+
+        root = bst.insert(root);
+        root = bst.insert(root);
+        root = bst.insert(root);
+        root = bst.insert(root);
+        root = bst.insert(root);
+
+        bst.minUsingInorder(root);
+        System.out.println("\nMin item is:  "+bst.min);
+
     }
 }
