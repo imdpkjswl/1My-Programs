0d3fc1d45a7291a9264061233d0a2580e6fa60b0
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-07-23 18:55:49 +0530

    some modification happened

---
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/11.Binary Search Tree/8.Lowest Common Ancestor in a Binary Search Tree/src/Main.java
@@ -0,0 +1,61 @@
+// A binary tree node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+
+    Node root;
+
+    /* Function to find LCA of n1 and n2. The function assumes that both n1 and n2 are present in BST */
+    Node lowestCommonAncestor(Node node, int n1, int n2) {
+        if (node == null)
+            return null;
+
+        // If both n1 and n2 are smaller than root, then LCA lies in left
+        if (node.data > n1 && node.data > n2)
+            return lowestCommonAncestor(node.left, n1, n2);
+
+        // If both n1 and n2 are greater than root, then LCA lies in right
+        if (node.data < n1 && node.data < n2)
+            return lowestCommonAncestor(node.right, n1, n2);
+
+        return node;
+    }
+
+    /* Driver program to test lca() */
+    public static void main(String args[]) {
+
+        // Let us construct the BST
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(20);
+        tree.root.left = new Node(8);
+        tree.root.right = new Node(22);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(12);
+        tree.root.left.right.left = new Node(10);
+        tree.root.left.right.right = new Node(14);
+
+        int n1 = 10, n2 = 14;
+        Node t = tree.lowestCommonAncestor(tree.root, n1, n2);
+        System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
+
+        n1 = 14;
+        n2 = 8;
+        t = tree.lowestCommonAncestor(tree.root, n1, n2);
+        System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
+
+        n1 = 10;
+        n2 = 22;
+        t = tree.lowestCommonAncestor(tree.root, n1, n2);
+        System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
+
+    }
+}
