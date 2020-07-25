1b4be51ad0f5c164826fa4ce5f716599db9646b2
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-07-26 00:02:13 +0530

    BFS Using list & inbuilt Queue

---
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/13.Basics of Graph/5.BFS Traversal Using List/src/Main.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    static int V;
+
+    static void addEdge(ArrayList<ArrayList<Integer>> graph, int u, int v) {
+        graph.get(u).add(v); // for directed
+    }
+
+    static void printList(ArrayList<ArrayList<Integer> > graph)
+    {
+        for (int i = 0; i < graph.size(); i++) {  // expanding Row
+            System.out.print(i+":  ");
+            for(int j=0;j<graph.get(i).size();j++){ // expanding column
+                System.out.print("  -> "+graph.get(i).get(j));
+            }
+            System.out.println();
+        }
+    }
+
+
+    static void bfs(ArrayList<ArrayList<Integer>> graph, int start) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[V];
+        q.add(start);
+        visited[start] = true;
+        System.out.print("->  "+start);
+
+        while(!q.isEmpty()) {
+            int u = q.poll();
+            for(int v =0; v <graph.get(u).size();v++){
+                int x = graph.get(u).get(v);
+                if(!visited[graph.get(u).get(v)]) {
+                    q.add(x);
+                    visited[x] = true;
+                    System.out.print(" ->  "+x);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        V = 5; // No. of vertex
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for(int i=0;i<V;i++)
+            graph.add(new ArrayList<>());  // Allocating memory for internal list
+
+
+        addEdge(graph,0,2);
+        addEdge(graph,1,0);
+        addEdge(graph,2,3);
+        addEdge(graph,3,1);
+        addEdge(graph,4,2);
+        addEdge(graph,3,4);
+        printList(graph);
+
+
+        int source = 0;
+        System.out.println("The BFS Traversal:");
+        bfs(graph, source);
+
+
+
+    }
+}
