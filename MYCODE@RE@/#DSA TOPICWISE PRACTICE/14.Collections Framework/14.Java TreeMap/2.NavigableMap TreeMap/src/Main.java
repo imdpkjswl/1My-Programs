93df52c45a726f92e8acb0502d3f66d95b8846c7
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-07-29 18:52:46 +0530

    Navigable

---
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/14.Collections Framework/14.Java TreeMap/2.NavigableMap TreeMap/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+class Main {
+    public static void main(String args[]) {
+
+        NavigableMap<Integer,String> map = new TreeMap<Integer,String>();
+        map.put(100,"Deepak");
+        map.put(102,"Ravi");
+        map.put(101,"Vijay");
+        map.put(103,"Rahul");
+
+        //Maintains descending order
+        System.out.println("descendingMap: "+map.descendingMap());
+
+        //Returns key-value pairs whose keys are less than or equal to the specified key.
+        System.out.println("headMap: "+map.headMap(102,true));
+
+        //Returns key-value pairs whose keys are greater than or equal to the specified key.
+        System.out.println("tailMap: "+map.tailMap(102,true));
+
+        //Returns key-value pairs exists in between the specified key.
+        System.out.println("subMap: "+map.subMap(100, false, 102, true));
+
+    }
+}  
