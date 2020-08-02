5d58dd12cf55923e8d0c93be813a70bc5841be12
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-08-02 12:33:21 +0530

    code simplified

--- MYCODE@RE@/#DSA TOPICWISE PRACTICE/15.Divide & Conquer/Standard Algorithms/2.Binary Search Using DAC/src/Main.java
+++ MYCODE@RE@/#DSA TOPICWISE PRACTICE/15.Divide & Conquer/Standard Algorithms/2.Binary Search Using DAC/src/Main.java
@@ -1,32 +1,32 @@
+// Prerequisite: Array must be sorted.
 public class Main {
 
     static int binary(int[] arr, int low, int high, int x) {
 
-        if(high >= low) {
-            int mid = low + (high-low) / 2;
-
-            if (arr[mid] == x)
-                return mid;
-
-            if (arr[mid] >= x)
-                return binary(arr, low, mid-1, x);
-
-            return binary(arr, mid + 1, high, x);
-        }
-        else
+        if(high < low)
             return -1;
+
+        int mid = (low+high) / 2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        if (x < arr[mid])
+            return binary(arr, low, mid-1, x);
+        else
+            return binary(arr, mid + 1, high, x);
 
     }
 
     public static void main(String[] args) {
 
-        int[] arr = {5, 9, 3, -4, 7, 3, 2, 12, 5, 1};
+        int[] arr = {2, 4, 6, 14, 17, 23, 26, 32, 45, 51};
         int n = arr.length;
-        int x = 5;
+        int x = 45;
 
         int result = binary(arr, 0, n-1, x);
         if(result != -1)
-            System.out.println("Key found");
+            System.out.println("Key found at "+result);
         else
             System.out.println("Key not found");
 
