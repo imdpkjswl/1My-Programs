a7ff0feb3f4eae2586582c392b67ef1e4025abfb
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-08-27 11:29:14 +0530

    Google IT Support Automation with python two course completed

---
+++ MYCODE@RE@/Competitive Programming/CPP Programs/#Learn_STL.cpp
@@ -0,0 +1,19 @@
+#define INPUT_OUTPUT { \
+	freopen("input.txt", "r", stdin); \
+	freopen("output.txt", "w", stdout);\
+}
+
+// CPP PROGRAM STARTS HERE
+#include<bits/stdc++.h>
+using namespace std;
+
+
+signed main() {
+	INPUT_OUTPUT; // HANDLING INPUT OUTPUT
+
+	cout << "Learn C++ STL" << endl;
+
+
+
+	return 0;
+}
