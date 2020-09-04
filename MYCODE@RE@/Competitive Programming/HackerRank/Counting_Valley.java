ff15fc8516a3db948e4253f038d4f9d6ef70f836
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-09-04 16:54:43 +0530

    D

---
+++ MYCODE@RE@/Competitive Programming/HackerRank/Counting_Valley.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Counting_Valley {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(String str) {
+        int altitude = 0;
+        int valley = 0;
+
+        char[] chr = str.toCharArray(); // convert string to char array
+
+        for (char steps : chr) { // pick one charcter one by one and decide altitude
+            if (steps == 'D')
+                altitude--;
+            if (steps == 'U') {
+                altitude++;
+                if (altitude == 0)
+                    valley++;  // got the sea level
+            }
+        }
+
+        return valley;
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        String string = "DDUUDDUDUUUD";
+        int result = countingValleys(string);
+
+        System.out.println(result);
+
+    }
+}
