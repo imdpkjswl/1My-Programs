4c462a24239c3884f223627c9cf89a0cf8e36288
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-09-14 20:52:52 +0530

    *MUST TRY

---
+++ MYCODE@RE@/Competitive Programming/HackerRank/Bigger_is_Greater.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Bigger_is_Greater {
+
+	// Complete the biggerIsGreater function below.
+	static String biggerIsGreater(String w) {
+
+
+	}
+
+
+	public static void main(String[] args) throws IOException {
+
+		String w = "hefg"
+
+		           String result = biggerIsGreater(w);
+		System.out.println(result);
+
+	}
+}
