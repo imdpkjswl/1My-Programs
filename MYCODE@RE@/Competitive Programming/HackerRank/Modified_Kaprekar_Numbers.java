2dc16fe44b2081212b0f40f15675cd279964325d
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-09-15 22:17:24 +0530

    Modified_Kaprekar_Numbers

--- MYCODE@RE@/Competitive Programming/HackerRank/Modified_Kaprekar_Numbers.java
+++ MYCODE@RE@/Competitive Programming/HackerRank/Modified_Kaprekar_Numbers.java
@@ -1,45 +1,43 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
+import java.text.*;
+import java.math.*;
 import java.util.regex.*;
 
 public class Modified_Kaprekar_Numbers {
-//kaprekarNumbers is: square of num and sum of right half and sum of left half should be equal to given number.
 
-	// Complete the kaprekarNumbers function below.
-	static void kaprekarNumbers(int p, int q) {
-		// 1/7 test cases got failed :)
-		boolean flag = false;
-
-		for (int i = p; i <= q; i++) {
-			long  sum;
-			long  a = 0;
-			long  k = i;
-			while (k > 0) {
-				k /= 10 ;
-				++a;
-			}
-			long  sq = i * i;
-			long  g = (long)Math.pow(10, a);
-			sum = sq % g + sq / g;
-			if (sum == i) {
-				System.out.print(sum + " ");
-				flag = true;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int p = scan.nextInt(); // begin
+		int q = scan.nextInt(); // end
+		String prefix = "";
+		for (int k = p; k <= q; k++) {
+			if (isKaprekar(k)) {
+				System.out.print(prefix + k);
+				prefix = " ";
 			}
 		}
-		if (!flag)
+		if (prefix.equals(""))
 			System.out.println("INVALID RANGE");
 	}
 
-	private static final Scanner scanner = new Scanner(System.in);
-
-	public static void main(String[] args) {
-		int lower = 1;
-		int upper = 100;
-		kaprekarNumbers(lower, upper);
-
+	public static boolean isKaprekar(long k) {
+		if (k < 10)
+			return (k == 1 || k == 9);
+		long square = k * k;
+		String str = String.valueOf(square);
+		int d = String.valueOf(k).length();
+		int first, second;
+		try {
+			first = Integer.parseInt(str.substring(0, str.length() - d));
+			second = Integer.parseInt(str.substring(str.length() - d));
+		} catch (Exception e) {
+			System.out.println("\n" + k + ", " + square + ", " + str.length() + ", " + d);
+			throw e;
+		}
+		if (first + second == k) {
+			return true;
+		}
+		return false;
 	}
 }
