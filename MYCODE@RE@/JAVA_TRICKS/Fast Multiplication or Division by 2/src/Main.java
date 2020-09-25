8e5243b25b241b9eba392a65f220fa303b43d2a7
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-09-25 18:43:11 +0530

    trick3

---
+++ MYCODE@RE@/JAVA_TRICKS/Fast Multiplication or Division by 2/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 8;
+
+        n = n << 1; // Multiply n with 2
+        System.out.println(n);
+
+        n = 8;
+        n = n >> 1; // Divide n by 2
+        System.out.println(n);
+    }
+}
