69472e9c4289004e587ee6ef26e211229c7a1648
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-10-31 22:53:21 +0530

    my new solution-added

--- MYCODE@RE@/Competitive Programming/LeetCode/_26_Remove_Duplicates_from_Sorted_Array.java
+++ MYCODE@RE@/Competitive Programming/LeetCode/_26_Remove_Duplicates_from_Sorted_Array.java
@@ -1,22 +1,27 @@
 import java.util.*;
 
 public class _26_Remove_Duplicates_from_Sorted_Array{
-	// Apply Two pointer algo
 	public int duplicate(int[] nums) {
-		if(nums.length == 0)
-			return 0;
-		
-		int count=0;
-		for(int i=1;i<nums.length;++i){
-			if(nums[i] != nums[count]){
-				count++; // counting no. of unique elements in nums
-                nums[count] = nums[i]; // shift
-			}
-		}
-		// for(int i:nums)
-		// 	System.out.println(i+"--->");
-		return count+1;
-		
+		    int n = nums.length;
+        ArrayList<Integer> temp = new ArrayList<>();
+        if(n == 0)
+            return 0;
+        
+        
+        temp.add(nums[0]);
+        int count = 1;
+        for(int i=1;i<n;i++){
+            if(nums[i-1] != nums[i]){
+                temp.add(nums[i]);
+                count++;
+            }
+        }
+        
+        for(int i=0;i<count;i++){
+            nums[i] = temp.get(i);
+        }
+        return count;
+        // Note: (count)th no. of element must be sorted in original array.
     }
 
 	public static void main(String[] args){
