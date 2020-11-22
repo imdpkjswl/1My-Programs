a7bd4431c8834686e05186891edbc53b28e2f577
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-11-22 12:03:36 +0530

    interesting easy problem-TLE Covered

--- MYCODE@RE@/Competitive Programming/BinarySearch/Closest_Distance_to_Character.java
+++ MYCODE@RE@/Competitive Programming/BinarySearch/Closest_Distance_to_Character.java
@@ -35,11 +35,59 @@ class Closest_Distance_to_Character {
         String s = "aabaab";
         String c = "b";
 
+        // Solution with TLE
         int[] ans = run.solve(s, c);
 
         for(int i:ans)
-            System.out.print(i+ "  ");
+            System.out.print(i + "  ");
+        System.out.println();
 
+
+        // Real Solution without TLE
+        Solution real = new Solution();
+        int[] a = real.solve(s, c);
+        for(int val : a)
+        	System.out.print(val + "  ");
 
     }
+}
+
+
+
+
+// REAL SOLUTION WITHOUT TLE ERROR
+class Solution {
+    public int[] solve(String s, String c) {  
+        int n = s.length();
+        int[] ans = new int[n];
+        char X = c.charAt(0);
+   
+        // Find distance from occurrences of X  
+        // appearing before current character.  
+        int prev = Integer.MAX_VALUE;  
+        
+        for (int i = 0; i < n; i++)  
+        {  
+            if (s.charAt(i) == X)  
+                prev = i;  
+            if (prev == Integer.MAX_VALUE) 
+                ans[i] = Integer.MAX_VALUE; 
+            else    
+                ans[i] = (i - prev);  
+        }  
+      
+        // Find distance from occurrences of X  
+        // appearing after current character and  
+        // compare this distance with earlier.  
+        prev = Integer.MAX_VALUE; 
+        for (int i = n - 1; i >= 0; i--)  
+        {  
+            if (s.charAt(i) == X)  
+                prev = i;  
+            if (prev != Integer.MAX_VALUE)     
+                ans[i] = Math.min(ans[i], prev - i);  
+        }  
+        
+        return ans;
+    }  
 }
