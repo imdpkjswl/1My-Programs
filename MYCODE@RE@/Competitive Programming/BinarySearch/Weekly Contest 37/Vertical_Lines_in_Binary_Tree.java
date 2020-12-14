3e0eb03f3939ec5f338e77af168a11e48adf2981
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2020-12-14 22:57:15 +0530

    ***VERY<---->IMP***

--- MYCODE@RE@/Competitive Programming/BinarySearch/Weekly Contest 37/Vertical_Lines_in_Binary_Tree.java
+++ MYCODE@RE@/Competitive Programming/BinarySearch/Weekly Contest 37/Vertical_Lines_in_Binary_Tree.java
@@ -5,10 +5,15 @@ We can track all of these horizontal offsets in a hash table and then return the
 final size of the hash table.
 
 Implementation:
-Our dfs function tracks, for the current node, its horizontal offset in the parameter d. When we go to the left, the horizontal offset decreases by 1, and when we go to the right, the horizontal offset increases by one. We throw all of these values into our unordered_set and then return the size of the set at the end.
+Our dfs function tracks, for the current node, its horizontal offset in the
+parameter  d.  When we go to the left, the horizontal offset decreases by 1,
+and when we go to the right, the horizontal offset increases by one. We throw
+all of these values into our unordered_set and then return the size of the set
+at the end.
 
 Time Complexity:
-The DFS takes linear time and the unordered set operations take constant time each, for a time complexity of \mathcal{O}(N)O(N).
+The DFS takes linear time and the unordered(hash) set operations take constant
+time each, for a time complexity of O(N).
 
 Space Complexity:
 The answer can be N, so the space complexity is O(N).
@@ -49,11 +54,22 @@ class Vertical_Lines_in_Binary_Tree {
 		Tree root = null;
 		Vertical_Lines_in_Binary_Tree tree = new Vertical_Lines_in_Binary_Tree();
 
-		root = new Tree(1);
-		root.left = new Tree(2);
-		root.right = new Tree(3);
-		root.left.left = new Tree(4);
-		root.right.right = new Tree(5);
+		root  = new Tree(20);
+		root.left  = new Tree(10);
+		root.right  = new Tree(30);
+		root.left.left  = new Tree(5);
+		root.left.right  = new Tree(15);
+		root.right.left  = new Tree(25);
+		root.right.right  = new Tree(35);
+		root.left.left.left  = new Tree(3);
+		root.left.left.right  = new Tree(6);
+		root.left.right.left  = new Tree(13);
+		root.left.right.right  = new Tree(18);
+		root.right.left.left  = new Tree(22);
+		root.right.left.right  = new Tree(28);
+		root.right.right.left  = new Tree(32);
+		root.right.right.right  = new Tree(48);
+
 
 		int result = tree.solve(root);
 		System.out.println(result);
