94a99778e3166b7bb40824a7a2581493911140d4
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2021-01-20 01:44:19 +0530

    chessboard soln pending

---
+++ MYCODE@RE@/Competitive Programming/HackerEarth/January Circuits 21/A_chessboard.java
@@ -0,0 +1,48 @@
+/* IMPORTANT: Multiple classes and nested static classes are supported */
+
+/*
+ * uncomment this if you want to read input.
+//imports for BufferedReader
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//import for Scanner and other utility classes
+import java.util.*;
+*/
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+
+import java.util.*;
+// class TestClass {
+class A_chessboard {
+	public static void main(String args[] ) throws Exception {
+		/* Sample code to perform I/O:
+		 * Use either of these methods for input
+
+		//BufferedReader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String name = br.readLine();                // Reading input from STDIN
+		System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+		*/
+		//Scanner
+		Scanner s = new Scanner(System.in);
+		//String name = s.nextLine();                 // Reading input from STDIN
+		//System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
+
+
+
+		// Write your code here
+		//int[][] board = new int[8][8]; // chess board size
+		int k1p1, k1p2;
+		int k2p1, k2p2;
+		// Idea is to take diff of both pos must be 1, then decide who'd win.
+		for (int i = 1; i <= 8; i++) {
+			for (int j = 1; j <= 8; j++) {
+
+			}
+		}
+
+
+
+	}
+}
