8a5aff2de491f175304057db20afe266ea893076
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2021-01-20 20:02:54 +0530

    Rules:  The chessboard is a 8x8 standard  board. The kings can move from one cell to another if they have an edge or corner in common. If the generated coordinates of the second king are exactly equal to the first, then the second king wins. Both players play optimally. The game is considered to be won by the first king piece if after it moves, then it can have the same coordinates as the second. The second king piece becomes the winner if after this piece is moved, then both kings have the same coordinates. The game is drawn if there is no possible ways for anyone to win if both the pieces are played optimally.
    Rules:  The chessboard is a 8x8 standard  board. The kings can move from one cell to another if they have an edge or corner in common. If the generated coordinates of the second king are exactly equal to the first, then the second king wins. Both players play optimally. The game is considered to be won by the first king piece if after it moves, then it can have the same coordinates as the second. The second king piece becomes the winner if after this piece is moved, then both kings have the same coordinates. The game is drawn if there is no possible ways for anyone to win if both the pieces are played optimally.

--- MYCODE@RE@/Competitive Programming/HackerEarth/January Circuits 21/A_chessboard.java
+++ MYCODE@RE@/Competitive Programming/HackerEarth/January Circuits 21/A_chessboard.java
@@ -1,48 +1,32 @@
-/* IMPORTANT: Multiple classes and nested static classes are supported */
-
-/*
- * uncomment this if you want to read input.
-//imports for BufferedReader
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-//import for Scanner and other utility classes
 import java.util.*;
-*/
 
-// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-
-import java.util.*;
-// class TestClass {
 class A_chessboard {
-	public static void main(String args[] ) throws Exception {
-		/* Sample code to perform I/O:
-		 * Use either of these methods for input
+	private static void solve(int x1, int y1, int x2, int y2) {
+		if (((x2 - x1) == 0) && ((y2 - y1) == 0)) { // coordinates of the second king are exactly equal
+			System.out.println("SECOND"); // to the first, then the second king wins.
+		} else if ((Math.abs((x1 - x2)) <= 1) && (Math.abs((y1 - y2)) <= 1)) { // In one move, first king able to kill
+			System.out.println("FIRST");							// 	second king, hence first will win.
+		} else {
+			System.out.println("DRAW"); // both play optimally
+		}
+	}
 
-		//BufferedReader
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String name = br.readLine();                // Reading input from STDIN
-		System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
-		*/
+	public static void main(String args[] ) throws Exception {
 		//Scanner
 		Scanner s = new Scanner(System.in);
-		//String name = s.nextLine();                 // Reading input from STDIN
-		//System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
-
-
 
 		// Write your code here
-		//int[][] board = new int[8][8]; // chess board size
-		int k1p1, k1p2;
-		int k2p1, k2p2;
-		// Idea is to take diff of both pos must be 1, then decide who'd win.
-		for (int i = 1; i <= 8; i++) {
-			for (int j = 1; j <= 8; j++) {
+		int t;
+		t = s.nextInt();
 
-			}
+		for (int i = 1; i <= t; i++) {
+			int x1 = s.nextInt();
+			int y1 = s.nextInt();
+			int x2 = s.nextInt();
+			int y2 = s.nextInt();
+
+			solve(x1, y1, x2, y2);
 		}
-
-
 
 	}
 }
