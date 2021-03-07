02cfdc577a17cdf1e56cf4aeb9ffb7b2b7ea1597
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2021-03-07 16:19:16 +0530

    Partially solved- Getting TLE Error - NEED to solve in O(N), Not in O(N^2)

---
+++ MYCODE@RE@/Competitive Programming/CodeChef/MAR21C/Interesting_XOR.java
@@ -0,0 +1,34 @@
+import java.math.*;
+import java.util.*;
+import java.lang.*;
+
+class Interesting_XOR {
+
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int T = 0;if (in.hasNextInt()) {T = in.nextInt();}
+
+        for(int t=1;t<=T;t++){
+        long C = in.nextLong();
+
+        int d = 0;
+        while(C >= Math.pow(2,++d)){}
+        //System.out.println(d);
+        long n = (int)Math.pow(2,d);
+
+    	long max=0;
+    	for(long a=n-1;a>=1;a--){
+    		for(long b=a;b<n;b++){
+    			long xor = a^b;
+    			if(xor == C){
+    				max = Math.max(a*b, max);
+    				//System.out.println(a+"   "+b);
+    			}
+    		}
+    	}
+    	System.out.println(max);
+        }
+
+    }
+}
+
