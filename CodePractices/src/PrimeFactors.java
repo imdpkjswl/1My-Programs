4be2a5676f20f205140ff6e9edde6564a0aec406
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2021-09-08 00:41:52 +0530

    prime factors of number

--- CodePractices/src/PrimeFactors.java
+++ CodePractices/src/PrimeFactors.java
@@ -5,10 +5,7 @@ All Prime Factors of 51 are:
 3, 17
 */
 public class PrimeFactors {
-    public static void main(String[] args) {
-        int num = 51;
-
-        int[] arr = new int[num];
+    static void factors(int[] arr, int num) {
         int i=0, j=0;
 
         for(i=1;i<num/2;i++){
@@ -16,9 +13,17 @@ public class PrimeFactors {
                 arr[j++] = i; // store factors
             }
         }
+    }
 
-        for(i=1;i<num;i++){
-            if(arr[i] !=0) {
+    public static void main(String[] args) {
+        int num = 51;
+
+        int[] arr = new int[num];
+
+        factors(arr, num);
+
+        for(int i=1;i<num;i++){
+            if(arr[i] !=0) { // print factors except 1 and num
                 System.out.print(arr[i] + " ");
             }
         }
