ebdb12e4de4a05883dd06b7b9e72b7c86423684d
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2021-09-07 21:24:28 +0530

    LCM of two number

--- CodePractices/src/LCM.java
+++ CodePractices/src/LCM.java
@@ -1,5 +1,24 @@
 public class LCM {
-    public static void main(String[] args) {
-        
+
+    static void lcm(int n1, int n2) {
+        // maximum of n1 and n2 is stored in lcm
+        int lcm = (n1 > n2) ? n1 : n2;
+
+        while (true) {
+            if (lcm % n1 == 0 && lcm % n2 == 0) {
+                System.out.printf("The LCM of %d and %d is:  %d", n1, n2, lcm);
+                break;
+            }
+            lcm++; // after each comparison increment lcm value by ONE
+        }
+
+    }
+
+
+    public static void main(String... args) {
+        int n1 = 3;
+        int n2 = 4;
+
+        lcm(n1, n2);
     }
 }
