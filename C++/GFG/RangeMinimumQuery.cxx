9b6a6371366dd930b4ce5ca3fd242d13c1672046
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2021-09-20 00:24:01 +0530

    naive approach used, need lookup table approach

---
+++ C++/GFG/RangeMinimumQuery.cxx
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[] = {7, 2, 3, 0, 5, 10, 3, 12, 18};
+    
+    int query1[] = {0, 4};
+    int query2[] = {4, 7}; 
+    int query3[] = {7, 8};
+
+    // Naive solution
+    int min = 999999999;
+    for(int i=query1[0];i<=query1[1];i++){
+        if(a[i]<min){
+            min = a[i];
+        }
+    }
+
+    cout<<min<<endl;
+
+    return 0;
+}
