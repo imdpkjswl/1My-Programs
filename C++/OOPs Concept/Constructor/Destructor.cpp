9ed95bff62d67fbf167b73995178b4d64117eb4a
DEEPAK JAISWAL <43009486+deepakjaiswal2018@users.noreply.github.com>
2021-10-13 14:32:57 +0530

    Destructor learnt

---
+++ C++/OOPs Concept/Constructor/Destructor.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;  
+class Employee  
+{  
+   public:  
+        Employee()    
+        {    
+            cout<<"Constructor Invoked"<<endl;    
+        }    
+        
+        ~Employee()    
+        {    
+            cout<<"Destructor Invoked"<<endl;    
+        }  
+};  
+
+int main(void)   
+{  
+    Employee e1;   
+    Employee e2; 
+
+    return 0;  
+}  
